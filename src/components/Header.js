e76632d53ce8d1035c198ada24e462ea6828d2bb
Svyatoslav <sheinsvyatoslav@gmail.com>
2022-04-18 15:12:33 +0300

    frontend added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,42 @@
+import React, {useState} from 'react';
 import headerLogo from '../images/header_logo.svg';
 import NavTab from './NavTab'
-import { useLocation } from "react-router";
+import { useLocation } from "react-router-dom";
 import { useMediaQuery } from 'react-responsive'
+import unwrapIcon from '../images/unwrap_icon.svg'
+import closeIcon from '../images/close_icon.svg'
 
 export default function Header() {
   const location = useLocation();
   const isBigScreen = useMediaQuery({ query: '(min-width: 1024px)' })
+  const [navBarOpen, setNavBarOpen] = useState(false);
+
+  function handleNavBarOpen() {
+    setNavBarOpen(!navBarOpen)
+  }
 
   return (
-    <header className="header" style={{
-      backgroundColor: location.pathname === '/' ? '#073042' : 'white'
-      }}>
-      <div className="header__container">
-        <img className="header__image" src={headerLogo} alt='Логотип проекта' />
-        {isBigScreen
-        ? <NavTab />
-        : <button
-        className="header__wrap-button"
-        type="button"
-      />
-        }
-      </div>
-    </header>
+    <>
+      {!isBigScreen && navBarOpen && <NavTab isOpen={navBarOpen}/>}
+      <header className="header" style={{
+        backgroundColor: location.pathname === '/' ? '#073042' : 'white'
+        }}>
+        <div className="header__container">
+          <img className="header__image" src={headerLogo} alt='Логотип проекта' />
+          {isBigScreen || location.pathname === '/'
+          ? <NavTab />
+          : <button
+          onClick={handleNavBarOpen}
+          className="header__wrap-button"
+          type="button"
+          style={{
+            backgroundImage: `url(${navBarOpen ? closeIcon : unwrapIcon})`
+          }}
+        />
+          }
+        </div>
+      </header>
+    </>
+
   )
 }
--- src/components/NavTab.js
+++ src/components/NavTab.js
@@ -1,10 +1,8 @@
-import { Switch, Route, Link, NavLink } from 'react-router-dom';
-import { useLocation } from "react-router";
+import { useLocation, Switch, Route, Link, NavLink } from 'react-router-dom';
 import profileIcon from '../images/user_icon.svg';
 
-export default function NavTab() {
+export default function NavTab(props) {
   const location = useLocation();
-
 
   function renderNavBar() {
     return (
@@ -16,6 +14,7 @@ export default function NavTab() {
           </Link>
         </Route>
         <Route path={["/movies", "/saved-movies", "/profile"]}>
+          <div className={`navtab__container ${props.isOpen ? "navtab__container_opened" : ''}`}>
             <div className="navtab__movies">
               <NavLink className="navtab__link" activeClassName="navtab__link_active" to="/movies">Фильмы</NavLink>
               <NavLink className="navtab__link" activeClassName="navtab__link_active" to="/saved-movies" style={{marginLeft: '16px'}}>Сохранённые фильмы</NavLink>
@@ -24,6 +23,7 @@ export default function NavTab() {
               <Link className="navtab__link navtab__link_active" to="/profile">Аккаунт</Link>
               <img className="navtab__profile-icon" src={profileIcon} alt="Иконка профиля"/>
             </div>
+          </div>
         </Route>
       </Switch>
     )
