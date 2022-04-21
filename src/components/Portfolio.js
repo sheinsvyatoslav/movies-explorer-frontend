5a437b1261ac55337aa2b131de0ebc52fd31a623
Svyatoslav <sheinsvyatoslav@gmail.com>
2022-04-21 19:10:38 +0300

    bugs fixed

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,15 +6,15 @@ export default function Portfolio() {
       <p className="portfolio__title">Портфолио</p>
         <div className="portfolio__line">
           <p className="portfolio__item">Статичный сайт</p>
-          <a className="portfolio__link" href="https://github.com/sheinsvyatoslav/how-to-learn"><img className="portfolio__link-icon" src={linkIcon} alt="Иконка ссылки"/></a>
+          <a className="portfolio__link" href="https://github.com/sheinsvyatoslav/how-to-learn"><img className="portfolio__link-icon" src={linkIcon} alt="Иконка ссылки" target="_blank"/></a>
         </div>
         <div className="portfolio__line">
           <p className="portfolio__item">Адаптивный сайт</p>
-          <a className="portfolio__link" href="https://github.com/sheinsvyatoslav/russian-travel"><img className="portfolio__link-icon" src={linkIcon} alt="Иконка ссылки"/></a>
+          <a className="portfolio__link" href="https://github.com/sheinsvyatoslav/russian-travel"><img className="portfolio__link-icon" src={linkIcon} alt="Иконка ссылки" target="_blank"/></a>
         </div>
         <div className="portfolio__line">
           <p className="portfolio__item">Одностраничное приложение</p>
-          <a className="portfolio__link" href="https://github.com/sheinsvyatoslav/react-mesto-api-full"><img className="portfolio__link-icon" src={linkIcon} alt="Иконка ссылки"/></a>
+          <a className="portfolio__link" href="https://github.com/sheinsvyatoslav/react-mesto-api-full"><img className="portfolio__link-icon" src={linkIcon} alt="Иконка ссылки" target="_blank"/></a>
         </div>
     </section>
   )
